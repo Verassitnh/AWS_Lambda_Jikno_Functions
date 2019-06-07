e637d6c89f6b6e18a053f68b35bd94c3e6adac9d
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-07 14:50:30 -0500

    Create GetAllApps.js

---
+++ API/Read/GetAllApps.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async (e, context) => {
+	const documentClient = new AWS.DynamoDB.DocumentClient();
+
+	let resBody = "";
+	let statCode = 0;
+
+	const params = {
+		TableName: "Library"
+	};
+
+
+	try {
+		const data = await documentClient.scan(params).promise();
+		resBody = JSON.stringify(data.Items);
+		statCode = 200;
+	} catch (err) {
+		resBody = `Unable to Get Users: ${err} `;
+		statCode = 500;
+	}
+
+	const res = {
+		statCode,
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: resBody,
+	};
+	return res;
+};
