cec9d6112992cb44c53828e740d188e358a30367
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-07 17:35:44 -0500

    Create CreateVersion.js

---
+++ API/Create/CreateVersion.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+	const documentClient = new AWS.DynamoDB.DocumentClient();
+
+	let resBody = "";
+	let statCode = 0;
+console.log('EVENT = '+ JSON.stringify(event));
+    const { version, discordIcon, discordInvite, frontend, backend, changeLog } = event;
+
+
+
+	const params = {
+		TableName: "About",
+		Item: {
+			 version,
+			 discordIcon,
+			 discordInvite,
+			 frontend,
+			 backend,
+			 changeLog,
+		}
+	};
+	try {
+		const data = await documentClient.put(params).promise();
+		resBody = JSON.stringify(data);
+		statCode = 201;
+
+	} catch (err) {
+		resBody = `Unable to Create App: ${err} `;
+		statCode = 403;
+	}
+
+	const res = {
+		statCode,
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: resBody,
+	};
+	return res;
+};
