e1bc2a0f0380989d33fc40cffee6487276427a4d
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-07 20:58:00 -0500

    Create GetSingleApp.js

---
+++ API/Read/GetSingleApp.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+	const documentClient = new AWS.DynamoDB.DocumentClient();
+
+	const { appName } = event.pathParameters;
+
+	let resBody = "";
+	let statCode = 0;
+
+	const params = {
+		TableName: "Library",
+		Key: {
+			appName: appName,
+		}
+	};
+
+
+	try {
+		const data = await documentClient.get(params).promise();
+		resBody = JSON.stringify(data);
+		statCode = 200;
+
+	} catch (err) {
+		resBody = `Unable to GET App: ${err} `;
+		statCode = 500;
+	}
+
+	const res = {
+		statusCode: statCode,
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: resBody,
+	};
+	return res;
+};
