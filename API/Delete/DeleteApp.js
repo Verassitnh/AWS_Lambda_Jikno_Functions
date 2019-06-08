3becaef0681bd35fab0eafcb7615e6da772f74d6
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-07 21:15:02 -0500

    Create DeleteApp.js

---
+++ API/Delete/DeleteApp.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+	const documentClient = new AWS.DynamoDB.DocumentClient();
+
+	let resBody = "";
+	let statCode = 0;
+
+
+	const { appName  } = event.pathParameters;
+
+	const params = {
+		TableName: "Library",
+		Key: {
+			appName,
+		}
+	};
+
+
+	try {
+		const data = await documentClient.delete(params).promise();
+		resBody = JSON.stringify(data);
+		statCode = 204;
+
+	} catch (err) {
+		resBody = `Unable to Delete User: ${err} `;
+		statCode = 403;
+	}
+
+	const res = {
+		statusCode: statCode,
+		headers: {
+			"Content-Type": "application/json",
+			"access-control-allow-origin": "*"
+		},
+		body: resBody,
+	};
+	return res;
+};
