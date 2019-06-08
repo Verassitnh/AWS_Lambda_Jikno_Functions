93142ffe180e9b254aeaf80bc7d5f58d991cb000
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-08 07:17:51 -0500

    Create CreatePalette.js

---
+++ API/Create/CreatePalette.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async(event, context) => {
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    let resBody = "";
+    let statCode = 0;
+
+
+
+    const params = {
+        TableName: "Palette",
+        Item: event
+    };
+    try {
+        const data = await documentClient.put(params).promise();
+        resBody = JSON.stringify(event);
+        statCode = 201;
+
+    }
+    catch (err) {
+        resBody = `Unable to Create Palette: ${err} `;
+        statCode = 403;
+    }
+
+    const res = {
+        statCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: resBody,
+    };
+    return res;
+};
