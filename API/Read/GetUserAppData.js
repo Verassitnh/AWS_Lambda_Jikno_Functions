1559552d01ce387fa034ca3f91b927e0a40e8487
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-08 06:57:49 -0500

    Create GetUserAppData.js

---
+++ API/Read/GetUserAppData.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+	const documentClient = new AWS.DynamoDB.DocumentClient();
+
+	const { userID } = event.pathParameters;
+
+	let resBody = "";
+	let statCode = 0;
+
+	const params = {
+		TableName: "Apps",
+		Key: {
+			userID,
+		}
+	};
+
+
+	try {
+		const data = await documentClient.get(params).promise();
+		resBody = JSON.stringify(data);
+		statCode = 200;
+
+	} catch (err) {
+		resBody = `Unable to Get User: ${err} `;
+		statCode = 500;
+	}
+
+	const res = {
+		statusCode: statCode,
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: resBody,
+	};
+	return res;
+};
