723a9ff023c71d78203a9ce71aa230c51e676a92
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-06-08 10:41:19 -0500

    Update CreateUserAppData.js

--- API/Create/CreateUserAppData.js
+++ API/Create/CreateUserAppData.js
@@ -1,34 +1,25 @@
 'use strict';
 
-// WARNING: THIS FUNCTION NEEDS PARAMS
-
 const AWS = require('aws-sdk');
 
-exports.handler = async (event, context) => {
+exports.handler = async(event, context) => {
 	const documentClient = new AWS.DynamoDB.DocumentClient();
 
 	let resBody = "";
 	let statCode = 0;
-    // const { Put Something in here } = event;
 
 
-
-// 	const params = {
-// 		TableName: "Apps",
-// 		Item: {
-//             do,
-//             something,
-//             fun,
-//             and,
-//             happy
-// 		}
-// 	};
+	const params = {
+		TableName: "Apps",
+		Item: event
+	};
 	try {
 		const data = await documentClient.put(params).promise();
-		resBody = JSON.stringify(event );
+		resBody = JSON.stringify(event);
 		statCode = 201;
 
-	} catch (err) {
+	}
+	catch (err) {
 		resBody = `Unable to Create AppData: ${err} `;
 		statCode = 403;
 	}
